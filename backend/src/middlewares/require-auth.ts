5748eaad3de473ed7dc11f2eee9728feeba5277d
Matan <ml.git201@gmail.com>
2025-06-03 14:05:57 +0300

    fix(deployment): fix

--- backend/src/middlewares/require-auth.ts
+++ backend/src/middlewares/require-auth.ts
@@ -1,11 +1,14 @@
 import type { NextFunction, Request, Response } from "express";
 import { NotAuthorizedError } from "../errors";
+import { getAuth } from "@clerk/express";
 
 export const requireAuth = (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
+  console.log(getAuth(req));
+  console.log(req.auth);
   if (!req.auth?.userId) {
     throw new NotAuthorizedError();
   }
