69f13724b275891ddb23e5a4d8a0960e4328966e
Matan <ml.git201@gmail.com>
2025-06-03 13:17:43 +0300

    fix(deployment): fix

--- frontend/src/api/api-client.ts
+++ frontend/src/api/api-client.ts
@@ -1,9 +1,6 @@
 import axios from "axios";
 
-const BACKEND_URL = import.meta.env.BACKEND_URL;
-
 export const api = axios.create({
-  baseURL: BACKEND_URL ?? "http://localhost:3000",
   headers: { "Content-Type": "application/json" },
   withCredentials: true,
 });
